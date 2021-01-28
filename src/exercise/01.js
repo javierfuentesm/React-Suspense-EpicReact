60efec73e5abcc979617900495cf12f652406b24
Javier <javier.fuentesm@hotmail.com>
2021-01-28 07:06:19 -0600

    React Suspense first exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,30 +4,17 @@
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
 import {PokemonDataView, fetchPokemon} from '../pokemon'
-
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
-// 🐨 create a variable called "pokemon" (using let)
 let pokemon
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
 const pokemonPromise = fetchPokemon('pikachu').then(
   response => (pokemon = response),
 )
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
   if (!pokemon) {
     throw pokemonPromise
   }
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
