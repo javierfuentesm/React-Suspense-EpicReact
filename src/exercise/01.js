ece4c60cfec40eea68c5b59d8ad2b03fe69c7870
Javier <javier.fuentesm@hotmail.com>
2021-02-02 07:05:39 -0600

    Migrating from a useeffect to a react suspense

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,52 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView, fetchPokemon} from '../pokemon'
-let pokemon
+import {
+  PokemonDataView,
+  fetchPokemon,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
 
-const pokemonPromise = fetchPokemon('pikachu').then(
-  response => (pokemon = response),
-)
+function createResource(promise) {
+  let status = 'pending'
+  let result = promise.then(
+    response => {
+      status = 'resolved'
+      result = response
+    },
+    error => {
+      status = 'rejected'
+      result = error
+    },
+  )
+
+  return {
+    read() {
+      if (status === 'pending') {
+        throw result
+      }
+      if (status === 'rejected') {
+        throw result
+      }
+      if (status === 'resolved') {
+        return result
+      }
+    },
+  }
+}
+
+const resource = createResource(fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  if (!pokemon) {
-    throw pokemonPromise
-  }
+  const pokemon = resource.read()
+
+  // if (pokemonError) {
+  //   throw pokemonError
+  // }
+  // if (!pokemon) {
+  //   throw pokemonPromise
+  // }
 
   return (
     <div>
@@ -29,8 +64,10 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={<h1>Loading...</h1>}>
-          <PokemonInfo />
+        <React.Suspense fallback={<PokemonInfoFallback name={'Pikachu'} />}>
+          <PokemonErrorBoundary>
+            <PokemonInfo />
+          </PokemonErrorBoundary>
         </React.Suspense>
       </div>
     </div>
